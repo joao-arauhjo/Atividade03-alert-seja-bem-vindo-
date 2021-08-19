33a764dadbb0ec91c797680d1047242ed111455d
JohnAra <87151137+joao-arauhjo@users.noreply.github.com>
2021-08-19 19:47:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+//Exercício 3 - Elabore um programa que recebe o nome de uma pessoa do terminal e mostra a seguinte mensagem: Olá {nome}! Seja bem vindo ao fantástico mundo da programação//
+let nome = prompt("Qual o seu nome?")
+alert("Olá, " +nome+ "! Seja bem vindo ao fantástico mundo da Blue :)")
+
+
